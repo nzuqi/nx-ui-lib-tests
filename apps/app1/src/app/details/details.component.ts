7bbf83fed76a724c5718e5f411c52eb9fc804bd5
Martin Nzuki <iamnzuki@gmail.com>
2025-01-08 10:58:41 +0300

    Render lib components

--- apps/app1/src/app/details/details.component.ts
+++ apps/app1/src/app/details/details.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { ButtonComponent, CardComponent } from '@nx-ui-lib-tests/components'
 
 @Component({
   selector: 'app-details',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent, CardComponent],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
 })
-export class DetailsComponent {}
+export class DetailsComponent {
+  currentRoute = signal<string>('intro');
+
+  constructor(private route: ActivatedRoute) {
+    this.handleCurrentRoute();
+  }
+
+  private handleCurrentRoute() {
+    this.route.params.subscribe(params => {
+      if (params['component'])
+        this.currentRoute.set(params['component']);
+    });
+  }
+}
