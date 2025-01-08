86ee8031a69e7d568c016e5ced1a6cd740444a9a
Martin Nzuki <iamnzuki@gmail.com>
2025-01-08 09:29:25 +0300

    Added dummy components

---
+++ libs/ui-components/src/lib/card/card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-card',
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss',
+})
+export class CardComponent {}
